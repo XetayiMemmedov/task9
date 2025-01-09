3d196c4fec4cf52cfec3c07b8bd5739d8e1ade07
XetayiMemmedov <khatayirm@code.edu.az>
2025-01-09 10:16:19 +0400

    Add project files.

---
+++ task9/Program.cs
@@ -0,0 +1,14 @@
+﻿//9cu tapsirigin helli
+Console.Write("Cumleni yaz:");
+int a = 0;
+string sentence = Console.ReadLine();
+for (int i = 0; i < sentence.Length; i++)
+{
+    if (sentence[i] == 'a' || sentence[i] == 'e' || sentence[i] == 'i' || sentence[i] == 'o' || sentence[i] == 'u')
+    {
+        a++;
+    }
+}
+Console.WriteLine($"Saitlerin sayi {a} ededdir");
+
+
