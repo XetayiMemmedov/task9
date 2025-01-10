dce4e360f58e05839f74ea45f10930eac058c5fd
XetayiMemmedov <khatayirm@code.edu.az>
2025-01-10 08:46:49 +0400

    input encoding correction

--- task9/Program.cs
+++ task9/Program.cs
@@ -1,10 +1,16 @@
 ﻿//9cu tapsirigin helli
+using System.Text;
+
+//Encoding.UTF8;
+Console.InputEncoding = Encoding.UTF8;
+Console.OutputEncoding = Encoding.UTF8;
 Console.Write("Cumleni yaz:");
 int a = 0;
 string sentence = Console.ReadLine();
+Console.WriteLine(sentence);
 for (int i = 0; i < sentence.Length; i++)
 {
-    if (sentence[i] == 'a' || sentence[i] == 'e' || sentence[i] == 'i' || sentence[i] == 'o' || sentence[i] == 'u')
+    if (sentence[i] == 'a' || sentence[i] == 'ə' || sentence[i] == 'ü' || sentence[i] == 'ı' || sentence[i] == 'i' || sentence[i] == 'o' || sentence[i] == 'u' || sentence[i]=='e')
     {
         a++;
     }
